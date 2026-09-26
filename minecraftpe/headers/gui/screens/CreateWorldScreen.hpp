9941882b2d87aecd6c66a3da13da81168701593e
RexC418 <barriossantiago397@gmail.com>
2026-09-26 14:57:29 -0400

    Restore seed mode fields in CreateWorldScreen

--- minecraftpe/headers/gui/screens/CreateWorldScreen.hpp
+++ minecraftpe/headers/gui/screens/CreateWorldScreen.hpp
@@ -32,6 +32,8 @@ struct CreateWorldScreen: SelectWorldScreen
 	MCOServerListItem field_170;
 	int32_t field_1B4;
 	std::shared_ptr<RestRequestJob> field_1B8;
+	Button* seedModeButton;
+	bool_t use64BitSeed;
 
 	CreateWorldScreen(CreateWorldScreenType, const MCOServerListItem&);
 	void closeScreen();
